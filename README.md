4a077cba97f67f2b5d952c847bc07e7a7c7d0aac
Jonathan Arnett <jonarnett90@gmail.com>
2015-03-29 10:55:03 -0400

    Working Devise

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ An INTERNET OF THINGS device that uses the CLOUD and BIG DATA to transfer VIRTUA
 ## TODO
 - BITCOIN API
 - Final send-off button. Asynchronous, please?
-- Full name and address do not carry through registration
 
 ### Totally stretch
 - API keys for devices
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:address, :full_name, :guid) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :address, :full_name, :guid)  }
   end
 end
