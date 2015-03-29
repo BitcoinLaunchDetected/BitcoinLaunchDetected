14e7065d2e4ba245f567917ed6b577b30080bfe5
Jonathan Arnett <jonarnett90@gmail.com>
2015-03-29 01:37:50 -0400

    Sanitized parameters for full name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:address) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:address, :full_name) }
   end
 end
